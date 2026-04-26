e4a2427386cd8d95926018316c4e487689b653aa
Jesús Valero <jvalerodev@gmail.com>
2026-04-26 00:17:35 -0400

    feat: add smooth scroll to hash anchor on page load

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect } from 'react';
 import VisibilityState from '@/context/visibility/state';
 
 interface Props {
@@ -7,6 +8,16 @@ interface Props {
 }
 
 const Providers = ({ children }: Props) => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
+
   return <VisibilityState>{children}</VisibilityState>;
 };
 
